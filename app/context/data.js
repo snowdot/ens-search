3340ce65e8d5588f2dab689444d82470bb45247d
snowdot <co.pascu@gmail.com>
2023-12-08 19:44:15 +0100

    ✨ feat: add ENS to data provider

--- app/context/data.js
+++ app/context/data.js
@@ -1,4 +1,5 @@
-import { createContext } from "react";
+import { useState, useEffect, createContext } from "react";
+import { setupENS } from "@ensdomains/ui";
 
 export const DataContext = createContext();
 DataContext.displayName = "DataProvider";
@@ -8,8 +9,28 @@ export default function DataProvider({
   isDarkTheme,
   setIsDarkTheme,
 }) {
+  const [ens, setENS] = useState(undefined);
+
+  useEffect(() => {
+    const { ethereum } = window;
+    (async () => {
+      if (!ethereum) {
+        alert("Please install MetaMask");
+      }
+
+      const { ens } = await setupENS({
+        customProvider: ethereum,
+      });
+      setENS(ens);
+    })();
+
+    return () => {
+      setENS(null);
+    };
+  }, []);
+
   return (
-    <DataContext.Provider value={{ isDarkTheme, setIsDarkTheme }}>
+    <DataContext.Provider value={{ isDarkTheme, setIsDarkTheme, ens }}>
       {children}
     </DataContext.Provider>
   );
