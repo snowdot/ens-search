61b64942af613275bce03ce65d4689bc6e00acc4
snowdot <co.pascu@gmail.com>
2023-12-09 16:14:58 +0100

    ✨ feat: add meta url

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Providers } from "./providers";
 
 export const metadata = {
+  metadataBase: new URL("https://ens-identity.vercel.app/"),
   title: "ENS Identity Search",
   description: "ENS Identity Search is a festive, user-friendly tool that reveals your comprehensive Web3 identity, connecting your ENS profile with other digital identities",
 };
